c5d6bc7f8a65320c315fd75e67b8c34e777242ef
East-Robotics <robotsftc12656@gmail.com>
2024-02-12 14:40:06 -0900

    Create CenterStageDriveCode.java

---
+++ road-runner-quickstart-quickstart1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/CenterStageDriveCode.java
@@ -0,0 +1,228 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@TeleOp
+
+public class CenterStageDriveCode extends LinearOpMode {
+    private DcMotor LFMotor;
+    private DcMotor RFMotor;
+    private DcMotor LBMotor;
+    private DcMotor RBMotor;
+    private DcMotor IntakeRight;
+    private DcMotor RightArm;
+    private DcMotor LeftArm;
+    private Servo LeftWrist;
+    private Servo RightWrist;
+    private Servo TrapdoorL;
+    private Servo TrapdoorR;
+    private Servo Plane;
+
+    @Override
+    public void runOpMode() {
+        LFMotor = hardwareMap.get(DcMotor.class, "LFMotor");
+        RFMotor = hardwareMap.get(DcMotor.class, "RFMotor");
+        LBMotor = hardwareMap.get(DcMotor.class, "LBMotor");
+        RBMotor = hardwareMap.get(DcMotor.class, "RBMotor");
+        IntakeRight = hardwareMap.get(DcMotor.class, "IntakeRight");
+        RightArm = hardwareMap.get(DcMotor.class, "RightArm");
+        LeftArm = hardwareMap.get(DcMotor.class, "LeftArm");
+        LeftWrist = hardwareMap.get(Servo.class, "LeftWrist");
+        RightWrist = hardwareMap.get(Servo.class, "RightWrist");
+        Plane = hardwareMap.get(Servo.class, "Plane");
+        TrapdoorL = hardwareMap.get(Servo.class, "TrapdoorL");
+        TrapdoorR = hardwareMap.get(Servo.class, "TrapdoorR");
+        LBMotor.setDirection(DcMotor.Direction.REVERSE);
+        LFMotor.setDirection(DcMotor.Direction.REVERSE);
+        IntakeRight.setDirection(DcMotor.Direction.FORWARD);
+        RightWrist.setDirection(Servo.Direction.REVERSE);
+        boolean rightwristIsOpen = true;
+        boolean leftwristIsOpen = true;
+        boolean intakerun = true;
+        boolean LtrapdoorIsOpen = true;
+        boolean RtrapdoorIsOpen = true;
+        double Rightwristposition;
+        double Leftwristposition;
+        double Ltrapdoorposition;
+        double Rtrapdoorposition;
+
+        boolean currentAState = false;
+        boolean lastAState = false;
+        boolean lastXState = false;
+        boolean currentXState = false;
+        boolean lastLBState = false;
+        boolean currentYState = false;
+        boolean lastYState = false;
+        boolean currentLBState = false;
+//        boolean trapdoor = true;
+        Servo.Direction REVERSE;
+
+
+        telemetry.addData("Status", "Running");
+        telemetry.update();
+        waitForStart();
+//    public double PIDControl(double reference, double state){
+//
+//    }
+
+        while(opModeIsActive()) {
+            double RFtgtPower = 0;
+            double LFtgtPower = 0;
+            double RBtgtPower = 0;
+            double LBtgtPower = 0;
+            double py = -gamepad1.left_stick_y;
+            double px = gamepad1.left_stick_x;
+            double pa = gamepad1.right_stick_x;
+
+            LFMotor.setPower((py-px+pa) / 1.5);
+            RFMotor.setPower((py-px-pa) / 1.5);
+            LBMotor.setPower((py+px+pa) / 1.5);
+            RBMotor.setPower((py+px-pa) / 1.5);
+
+            currentAState = gamepad2.a;
+            currentXState = gamepad2.x;
+            currentLBState = gamepad1.left_bumper;
+            currentYState = gamepad2.y;
+
+
+
+            if (gamepad1.dpad_up){
+                LFMotor.setPower(0.7);
+                RFMotor.setPower(0.7);
+                LBMotor.setPower(0.7);
+                RBMotor.setPower(0.7);
+            }
+            if (gamepad1.dpad_down){
+                LFMotor.setPower(-0.7);
+                RFMotor.setPower(-0.7);
+                LBMotor.setPower(-0.7);
+                RBMotor.setPower(-0.7);
+            }
+            if (gamepad1.dpad_right){
+                LFMotor.setPower(-0.7);
+                RFMotor.setPower(0.7);
+                LBMotor.setPower(0.7);
+                RBMotor.setPower(-0.7);
+            }
+            if (gamepad1.dpad_left){
+                LFMotor.setPower(0.7);
+                RFMotor.setPower(-0.7);
+                LBMotor.setPower(-0.7);
+                RBMotor.setPower(0.7);
+            }
+            if (gamepad1.x){
+                Plane.setPosition(0);
+            }
+            else {
+                Plane.setPosition(1);
+            }
+
+            if(currentLBState && !lastLBState){
+                intakerun = !intakerun;
+            }
+
+            lastLBState = currentLBState;
+
+            if (intakerun){
+                IntakeRight.setPower(0);
+            }
+            else {
+                IntakeRight.setPower(0.6);
+            }
+
+            if(currentXState && !lastXState){
+                leftwristIsOpen = !leftwristIsOpen;
+                rightwristIsOpen = !rightwristIsOpen;
+            }
+
+            lastXState = currentXState;
+
+            if (leftwristIsOpen && rightwristIsOpen){
+                Leftwristposition = 0.23;
+                Rightwristposition = 0.23;
+
+            }
+            else{
+                Leftwristposition = 0.65;
+                Rightwristposition = 0.65;
+            }
+            LeftWrist.setPosition(Leftwristposition);
+            RightWrist.setPosition(Rightwristposition);
+
+
+            if (gamepad2.dpad_up){
+
+/*        int rotations = -5000;
+
+        RightArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        LeftArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        RightArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        LeftArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        RightArm.setTargetPosition(rotations);
+        LeftArm.setTargetPosition(rotations);
+
+        RightArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        LeftArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+ */
+                RightArm.setPower(-1);
+                LeftArm.setPower(-1);
+            }
+            else{
+                RightArm.setPower(0);
+                LeftArm.setPower(0);
+            }
+            if (gamepad2.dpad_down){
+/*                int rotations = 5000;
+
+        RightArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        LeftArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        RightArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        LeftArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        RightArm.setTargetPosition(rotations);
+        LeftArm.setTargetPosition(rotations);
+
+        RightArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        LeftArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+  */
+                RightArm.setPower(1);
+                LeftArm.setPower(1);
+            }
+            else{
+                RightArm.setPower(0);
+                LeftArm.setPower(0);
+            }
+            if(currentYState && !lastYState){
+//        LtrapdoorIsOpen = !LtrapdoorIsOpen;
+//        RtrapdoorIsOpen = !RtrapdoorIsOpen;
+            }
+
+            lastYState = currentYState;
+
+            if (LtrapdoorIsOpen && RtrapdoorIsOpen){
+                Ltrapdoorposition = 0.25;
+                Rtrapdoorposition = 0.25;
+
+            }
+            else{
+                Ltrapdoorposition = 0.75;
+                Rtrapdoorposition = 0.0;
+            }
+            TrapdoorL.setPosition(Ltrapdoorposition);
+            TrapdoorR.setPosition(Rtrapdoorposition);
+        }
+
+    }
+
+}
