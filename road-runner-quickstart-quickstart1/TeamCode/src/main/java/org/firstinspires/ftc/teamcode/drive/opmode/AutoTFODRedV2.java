d6a09d70317657cae3b2d95b09683894c13642e1
East-Robotics <155102765+East-Robotics@users.noreply.github.com>
2024-02-17 09:06:39 -0900

    webcam update

--- road-runner-quickstart-quickstart1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/AutoTFODRedV2.java
+++ road-runner-quickstart-quickstart1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/AutoTFODRedV2.java
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode.drive.opmode;
+
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
@@ -6,85 +7,53 @@ import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.tfod.TfodProcessor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
-
 
 import java.util.List;
 
 @Autonomous(name = "AutoTFODRedV2", group = "Concept")
-
 public class AutoTFODRedV2 extends LinearOpMode {
     private static final boolean USE_WEBCAM = true;
     private static final String TFOD_MODEL_ASSET = "AutoTFODRed.tflite";
-    private static final String[] LABELS = {
-            "RedProp",
-    };
+    private static final String[] LABELS = {"RedProp"};
     private TfodProcessor tfod;
     private VisionPortal visionPortal;
     private DcMotor LFMotor, RFMotor, LBMotor, RBMotor;
+    private SampleMecanumDrive drive;
     private ElapsedTime runtime = new ElapsedTime();
     private boolean redPropDetected = false;
-    private void driveCenterUsingOdometry() {
-        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-        // Drive backward for the specified distance using odometry pods
-        drive.followTrajectory(drive.trajectoryBuilder(new Pose2d())
-                .back(32.0)
-                .build()
-        );
-    }
 
-    private void driveBackwardAndTurnRight() {
-        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+    // Constants
+    private static final double BACKWARD_DISTANCE = 20.0;
+    private static final double TURN_ANGLE_RIGHT = -35.0;
+    private static final double TURN_ANGLE_LEFT = 35.0;
+    private static final double DRIVE_CENTER_DISTANCE = 20.0;
+    private static final double RESET_POSITION_DISTANCE = 7.5;
 
-        // Drive 20 inches backward
-        drive.followTrajectory(drive.trajectoryBuilder(new Pose2d())
-                .back(20.0)
-                .build()
-        );
-        // Turn right
-        drive.turn(Math.toRadians(-35)); // Turn right 30 degrees
-    }
+    private static final float MIN_CONFIDENCE = 0.45F;
+    private static final double RIGHT_SIDE_MIN_X = 345;
+    private static final double RIGHT_SIDE_MAX_X = 600;
+    private static final double LEFT_SIDE_MIN_X = 60;
+    private static final double LEFT_SIDE_MAX_X = 345;
 
-    private void driveBackwardAndTurnLeft() {
-        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-        // Drive 20 inches backward
-        drive.followTrajectory(drive.trajectoryBuilder(new Pose2d())
-                .back(20.0)
-                .build()
-        );
-        // Turn left
-        drive.turn(Math.toRadians(35)); // Turn left 30 degrees
-    }
-    private void driveCenterAfterUsingOdometry() {
-        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-        // Drive backward for the specified distance using odometry pods
-        drive.followTrajectory(drive.trajectoryBuilder(new Pose2d())
-                .back(20)
-                .build()
-        );
-    }
-    private void resetposition() {
-        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-        drive.followTrajectory(drive.trajectoryBuilder(new Pose2d())
-                .forward(7.5)
-                .build()
-        );
-        drive.turn(Math.toRadians(7.5));
-    }
     @Override
     public void runOpMode() throws InterruptedException {
+        // Motor initialization...
         LFMotor = hardwareMap.get(DcMotor.class, "LFMotor");
         RFMotor = hardwareMap.get(DcMotor.class, "RFMotor");
         LBMotor = hardwareMap.get(DcMotor.class, "LBMotor");
         RBMotor = hardwareMap.get(DcMotor.class, "RBMotor");
+
+        // Drive initialization...
+        drive = new SampleMecanumDrive(hardwareMap);
+
+        // TensorFlow initialization...
         initTfod();
+
         // Wait for the DS start button to be touched.
         telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
         telemetry.addData(">", "Touch Play to start OpMode");
@@ -98,22 +67,20 @@ public class AutoTFODRedV2 extends LinearOpMode {
                 // Push telemetry to the Driver Station.
                 telemetry.update();
                 // Share the CPU.
-                sleep(100);
+                sleep(10);
             }
         }
-        // Save more CPU resources when camera is no longer needed.
+        // Save more CPU resources when the camera is no longer needed.
         visionPortal.close();
-    }   // end runOpMode()
+    }
+
     private void initTfod() {
         // Create the TensorFlow processor by using a builder.
         tfod = new TfodProcessor.Builder()
                 .setModelAssetName(TFOD_MODEL_ASSET)
                 .setModelLabels(LABELS)
-                //.setIsModelTensorFlow2(true)
-                //.setIsModelQuantized(true)
-                //.setModelInputSize(300)
-                //.setModelAspectRatio(16.0 / 9.0)
                 .build();
+
         // Create the vision portal by using a builder.
         VisionPortal.Builder builder = new VisionPortal.Builder();
         if (USE_WEBCAM) {
@@ -121,61 +88,76 @@ public class AutoTFODRedV2 extends LinearOpMode {
         } else {
             builder.setCamera(BuiltinCameraDirection.BACK);
         }
-        // Choose a camera resolution. Not all cameras support all resolutions.
-        //builder.setCameraResolution(new Size(640, 480));
-        // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
-        //builder.enableLiveView(true);
-        // Set the stream format; MJPEG uses less bandwidth than default YUY2.
-        //builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
-        // Choose whether or not LiveView stops if no processors are enabled.
-        // If set "true", monitor shows solid orange screen if no processors enabled.
-        // If set "false", monitor shows camera view without annotations.
-        //builder.setAutoStopLiveView(false);
+
         // Set and enable the processor.
         builder.addProcessor(tfod);
+
         // Build the Vision Portal, using the above settings.
         visionPortal = builder.build();
+
         // Set confidence threshold for TFOD recognitions, at any time.
-        tfod.setMinResultConfidence(0.45f);
-        // Disable or re-enable the TFOD processor at any time.
-        // visionPortal.setProcessorEnabled(tfod, false);
-    }   // end method initTfod()
+        tfod.setMinResultConfidence(MIN_CONFIDENCE);
+    }
 
     private void telemetryTfod() {
         List<Recognition> currentRecognitions = tfod.getRecognitions();
         telemetry.addData("# Objects Detected", currentRecognitions.size());
 
         for (Recognition recognition : currentRecognitions) {
-            double x = (recognition.getLeft() + recognition.getRight()) / 2 ;
-            double y = (recognition.getTop()  + recognition.getBottom()) / 2 ;
-            telemetry.addData(""," ");
+            double x = (recognition.getLeft() + recognition.getRight()) / 2;
+            double y = (recognition.getTop() + recognition.getBottom()) / 2;
+
+            telemetry.addData("", " ");
             telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100);
             telemetry.addData("- Position", "%.0f / %.0f", x, y);
             telemetry.addData("- Size", "%.0f x %.0f", recognition.getWidth(), recognition.getHeight());
-            if (recognition.getConfidence() >= 0.45) {
+
+            if (recognition.getConfidence() >= MIN_CONFIDENCE) {
                 redPropDetected = true;
                 telemetry.addData("Red Prop Detected: ", "Confidence: %s", recognition.getConfidence());
-            } else {
-                telemetryTfod();
-            }
-            if (redPropDetected && (x > 345 && x < 600)) { //detects right side
-                telemetry.addData("Right", "");
-                resetposition();
-                driveBackwardAndTurnRight();
-                driveCenterAfterUsingOdometry();
-            } else if (redPropDetected && (x < 345 && x > 60)) { //detects left (center spike) side
-                telemetry.addData("Center", "");
-                resetposition();
-                driveCenterUsingOdometry();
-            } else{
-                telemetry.addData("Left", "");
-                resetposition();
-                driveBackwardAndTurnLeft();
-                driveCenterAfterUsingOdometry();
+
+                if (x > RIGHT_SIDE_MIN_X && x < RIGHT_SIDE_MAX_X) { // detects right side
+                    telemetry.addData("Right", "");
+                    resetposition();
+                    driveBackwardAndTurnRight();
+                    driveCenterAfterUsingOdometry();
+                } else if (x < LEFT_SIDE_MAX_X && x > LEFT_SIDE_MIN_X) { // detects left (center spike) side
+                    telemetry.addData("Center", "");
+                    resetposition();
+                    driveCenterUsingOdometry();
+                }
             }
         }
 
+        // Check if no recognitions meet the criteria for "Right" or "Center"
+        if (!redPropDetected) {
+            telemetry.addData("Left", "");
+            resetposition();
+            driveBackwardAndTurnLeft();
+            driveCenterAfterUsingOdometry();
+        }
     }
 
+    private void driveCenterUsingOdometry() {
+        drive.followTrajectory(drive.trajectoryBuilder(new Pose2d()).back(DRIVE_CENTER_DISTANCE).build());
+    }
 
-}   // end class
+    private void driveBackwardAndTurnRight() {
+        drive.followTrajectory(drive.trajectoryBuilder(new Pose2d()).back(BACKWARD_DISTANCE).build());
+        drive.turn(Math.toRadians(TURN_ANGLE_RIGHT));
+    }
+
+    private void driveBackwardAndTurnLeft() {
+        drive.followTrajectory(drive.trajectoryBuilder(new Pose2d()).back(BACKWARD_DISTANCE).build());
+        drive.turn(Math.toRadians(TURN_ANGLE_LEFT));
+    }
+
+    private void driveCenterAfterUsingOdometry() {
+        drive.followTrajectory(drive.trajectoryBuilder(new Pose2d()).back(DRIVE_CENTER_DISTANCE).build());
+    }
+
+    private void resetposition() {
+        drive.followTrajectory(drive.trajectoryBuilder(new Pose2d()).forward(RESET_POSITION_DISTANCE).build());
+        drive.turn(Math.toRadians(RESET_POSITION_DISTANCE));
+    }
+}
